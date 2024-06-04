909335469c8dc0072d72dde494f4be7209f78275
Seungbin Oh <sboh1214@gmail.com>
2024-06-04 22:56:31 +0900

    change to react 18 api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import TodoList from "./components/TodoList";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<TodoList />, rootElement);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<TodoList />);
